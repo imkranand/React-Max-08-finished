c78fa6dddac5ca13b32764693ed223282ea53fa9
Kumar Anand <kranand.vnit@gmail.com>
2024-01-21 17:24:47 +0530

    #20240121 - Optimization useCallback, react.memo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import  {Fragment } from 'react';
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
+import React, {useCallback, useState} from 'react';
+import Button from './components/UI/Button/Button';
+import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+  const [showParagraph, updateParagraph] = useState(false)
+  const [allowToggle, updateToggle] = useState(false);
+
+ const  toggleParagraphhandler = useCallback(() => {
+  console.log('clicked')
+  if (allowToggle) {
+    updateParagraph((prevState) => !prevState); 
+  }
+  },[allowToggle]);
+
+  const allowToggleHandler =() => {
+    console.log(allowToggle)
+    updateToggle((prevState) => !prevState);
+  }
   return (
-    <Fragment>
-        <Header></Header>
-        <main>
-          <Meals />
-        </main>
-    </Fragment>
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show = {showParagraph}></DemoOutput>
+      <Button onClick={allowToggleHandler}>Allow Toggle </Button>
+      <Button onClick={toggleParagraphhandler}>toggle paragraph </Button>
+    </div>
   );
 }
 
---
+++ src/components/Demo/DemoOutput.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function DemoOutput(props) {
+  return (
+    <div>{props.show ? 'This is a div':'' }</div>
+  )
+}
+
+export default DemoOutput
