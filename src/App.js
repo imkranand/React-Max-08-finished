36409f3ae4f4ddf07bbaeec693af7fca5cca69ae
Kumar Anand <kranand.vnit@gmail.com>
2024-01-28 12:50:23 +0530

    280124 - Initialization of the project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import React, {useCallback, useState} from 'react';
-import Button from './components/UI/Button/Button';
-import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
+import Users from './components/Users';
 
 function App() {
-  const [showParagraph, updateParagraph] = useState(false)
-  const [allowToggle, updateToggle] = useState(false);
-
- const  toggleParagraphhandler = useCallback(() => {
-  console.log('clicked')
-  if (allowToggle) {
-    updateParagraph((prevState) => !prevState); 
-  }
-  },[allowToggle]);
-
-  const allowToggleHandler =() => {
-    console.log(allowToggle)
-    updateToggle((prevState) => !prevState);
-  }
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show = {showParagraph}></DemoOutput>
-      <Button onClick={allowToggleHandler}>Allow Toggle </Button>
-      <Button onClick={toggleParagraphhandler}>toggle paragraph </Button>
+    <div>
+      <Users />
     </div>
   );
 }
--- src/components/Demo/DemoOutput.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function DemoOutput(props) {
-  return (
-    <div>{props.show ? 'This is a div':'' }</div>
-  )
-}
-
-export default DemoOutput
