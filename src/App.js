1c96da95f6031b1527547de9a681e664456474f4
Kumar Anand <kranand.vnit@gmail.com>
2024-03-11 17:34:34 +0530

    #11032024-Initial setup of the branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import Users from './components/Users';
-import UserFinder from './components/UserFinder';
+import React from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
   return (
-    <div>
-      {/* <Users /> */}
-      <UserFinder />
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 
