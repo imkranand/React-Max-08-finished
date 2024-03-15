f50fcbe9f604530d390b317c71c9da7b6f9d7332
Kumar Anand <kranand.vnit@gmail.com>
2024-03-15 20:41:31 +0530

    #15032024_2 - Base Code for Working with Form and User Input

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import React, { useEffect, useState } from 'react';
-
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
-import useHttp from './hooks/useHttp';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const url = 'https://react-http-33244-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json';
-
-  
-  const {isLoading, error, sendRequest: fetchTasks} = useHttp();
-
-  useEffect(() => {
-	const transformTask = (task) => {
-		const loadedTasks = [];
-	
-		for (const taskKey in task){
-		  loadedTasks.push({id: taskKey, text: task[taskKey].text});
-		}
-		setTasks(loadedTasks)
-	  };
-
-    fetchTasks({url:url},
-		transformTask);
-  }, [fetchTasks]);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
