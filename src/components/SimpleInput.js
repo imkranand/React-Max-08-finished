bf946543e888073ad7d1f112d0c93cda58718023
Kumar Anand <kranand.vnit@gmail.com>
2024-03-15 21:31:17 +0530

    #15032024_4 - Capturing input through useRef on submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   
   const [enteredName,setEnteredName] = useState('');
+  const nameInputRef = useRef();
 
   const nameChangeHandler = ($event) => {
     setEnteredName($event.target.value);
@@ -11,13 +12,22 @@ const SimpleInput = (props) => {
   const formSubmitHandler = ($event) => {
     $event.preventDefault();
 
-    console.log(enteredName);
+    console.log('useState',enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+
+    console.log('useRef', enteredValue);
   };
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameChangeHandler}/>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
