ee2fe527782b475925c7258ab10b746c559f51e0
Kumar Anand <kranand.vnit@gmail.com>
2024-03-17 17:57:57 +0530

    #17032024_4 - Setting multiple redux state values

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,8 +5,11 @@ import { useSelector,useDispatch } from 'react-redux';
 
 const Counter = () => {
   const counter = useSelector((state) =>  state.counter );
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
   const incrementHandler = () => {
     dispatch({type: 'inc'});
   };
@@ -15,12 +18,17 @@ const Counter = () => {
     dispatch({type: 'dec'});
   };
 
+  const customIncrementHandler =() => {
+    dispatch({type: 'increase', amount: 5})
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}> + </button>
+        <button onClick={customIncrementHandler}> increment by 5 </button>
         <button onClick={decrementHandler}> - </button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,28 @@ import {createStore } from 'redux';
 
 //creating a reducer function that needs to be passed while creating the store
 // reducer function should be created before being called in the next step of store creation
-const storeReducer = (state = {counter: 0}, action) => {
+const storeReducer = (state = {counter: 0, showCounter: true}, action) => {
+    // IMPORTANT NOTE: 1) While updating state set all the unchanged values to previous value and don't ignore any attribute stored in state. if we forget to include an attribute it will be set an undefined and that may lead to any issues or BUG
+    // 2) NEVER Modify existing state. Always set the new state to new object
     if(action.type === 'inc'){
         return{
             counter: state.counter + 1,
+            showCounter: state.showCounter,
         };
     } else if(action.type === 'dec'){
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter,
+        };
+    } else if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter,
+        };     
+    } else if (action.type === 'toggle') {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter,
         };
     }
     else {
