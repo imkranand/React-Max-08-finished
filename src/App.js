25cc3a4f40a7e18d7623d0334b391960c0e3db22
Kumar Anand <kranand.vnit@gmail.com>
2024-03-24 12:21:16 +0530

    #24032024_2 - Base code setup for the advance redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { Fragment } from 'react';
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
-import UserProfile from './components/UserProfile';
-import { useSelector } from 'react-redux';
-
-
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const isAuth = useSelector(state => state.auth.isAuthenticated)
   return (
-    <Fragment>
-      <Header></Header>
-      {!isAuth &&<Auth />}
-      {isAuth && <UserProfile />}
-      <Counter />
-    </Fragment>
-    
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
